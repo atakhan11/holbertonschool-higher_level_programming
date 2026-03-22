f2d1ca69d152ab0274c044a76dd74bca26b00734
atakhan11 <atakhanwork@gmail.com>
2026-03-22 19:19:57 +0000

    5-no_c.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    a = []
+    for i in my_string:
+        if i != "c" and i != "C":
+            a.append(i)
+    my_string = "".join(a)
+    return my_string
