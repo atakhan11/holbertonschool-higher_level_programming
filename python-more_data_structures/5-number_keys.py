3a7f38d0b8423455c20682d38d8b678d22450fa8
atakhan11 <atakhanwork@gmail.com>
2026-03-27 14:17:16 +0000

    5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    number = 0
+    key_list = []
+    for key in a_dictionary:
+        key_list.append(key)
+    number = len(key_list)
+    return number
