0dcaf72d1f9b8db617c755ba668f4c29d023a05c
atakhan11 <atakhanwork@gmail.com>
2026-03-29 11:03:05 +0000

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    '''Adding integer function'''
+    # Checking if a and b are float or int
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    # Casting to int if a and b is floating
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
