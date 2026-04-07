c0748900799cb313be68451ebdd800bfff7687c8
atakhan11 <atakhanwork@gmail.com>
2026-04-07 12:30:17 +0000

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This Module about subclass Square'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Class Square'''
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__:
+        return f"[Square] self.__{width}/self.__{height}"
