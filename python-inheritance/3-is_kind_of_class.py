65a6d0e7b18313c3e2dbb3ff02aafd91fa9da02b
atakhan11 <atakhanwork@gmail.com>
2026-04-07 10:18:53 +0000

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module checks Same class or inherit from'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Function for checking of same class or inherited'''
+    return isinstance(obj, a_class)
