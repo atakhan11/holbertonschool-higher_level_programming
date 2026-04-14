e78ce3a8a09c5498383ea097adbdb97694f40d3a
atakhan11 <atakhanwork@gmail.com>
2026-04-14 13:16:23 +0000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''This module adds all arguments to a Python list, and then save them to a file'''
+'''Module for file'''
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -8,7 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     items = load_from_json_file(filename)
-except:
+except Exception:
     items = []
 items.extend(sys.argv[1:])
 save_to_json_file(items, filename)
