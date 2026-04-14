9157221789098d97dc001584f55fed79a339cc11
atakhan11 <atakhanwork@gmail.com>
2026-04-14 12:49:09 +0000

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This module creates object from a JSON file'''
+import json
+
+
+def load_from_json_file(filename):
+    '''Create object'''
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(f)
