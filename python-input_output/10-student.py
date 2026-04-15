0605b95720ba74c1089a0fb6a3876795f5501b4e
atakhan11 <atakhanwork@gmail.com>
2026-04-15 11:17:37 +0000

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''This module about class to JSON with filter'''
+
+
+class Student:
+    '''Student Class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        if attrs is None:
+            return self.__dict__
+        for key in attrs:
+            if key in self.__dict__keys():
+                new_dict[key] = self.__dict__[key]
+        return new_dict
