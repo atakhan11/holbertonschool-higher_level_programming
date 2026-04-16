9a403bcfccf40678d7b2dc0a1d07505cb232bcb3
atakhan11 <atakhanwork@gmail.com>
2026-04-16 10:03:45 +0000

    Pickling Custom Classes

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
-'''This module about is Pickling custom Classes'''
+"""Module for serialize and deserialize Python objects"""
 import pickle
 
 
 class CustomObject:
-    '''Class CustomObject'''
+    """The CustomObject class"""
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
-
-
+    
+    
     def display(self):
-        '''Display method for print object's attributes'''
-        print({self.name})
-        print({self.age})
-        print({self.is_student})
-
+        print(f"Name: {self.name}\n"
+              f"Age: {self.age}\n"
+              f"Is Student: {self.is_student}")
+    
 
     def serialize(self, filename):
-        '''Serialize method'''
-        with open(filename, 'w', encoding="utf-8") as file:
-            pickle.dump(self, file)
-
-    @classmethod
+        """Function to serialize and save Instance to a file."""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+     
+    
+    @classmethod 
     def deserialize(cls, filename):
-        '''Deserialize method'''
-        with open(filename, 'r', encoding="utf-8") as file:
-            return pickle.load(cls, file)
+        """Function to load and deserialize data from a file."""
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except Exception:
+            return None
