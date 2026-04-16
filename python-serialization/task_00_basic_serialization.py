a660fba697304a8f1b265363fc9729d7c6b2bc2b
atakhan11 <atakhanwork@gmail.com>
2026-04-16 09:33:04 +0000

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,10 +5,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     '''Serialization function'''
-    with open(filname, 'w') as file:
+    with open(filname, 'w', encoding="utf-8") as file:
         json.dump(data, file)
+
 
 def load_and_deserialize(filename):
     '''Deserialization function'''
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         return json.load(file)
