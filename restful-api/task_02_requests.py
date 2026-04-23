74f1dd0a9ba996fa2797daaa10f55c0bd7fa8422
atakhan11 <atakhanwork@gmail.com>
2026-04-23 12:18:58 +0000

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,7 +4,7 @@ import csv
 URL = "https://jsonplaceholder.typicode.com/posts"
 
 def fetch_and_print_posts():
-    respons = requests.get(URL)
+    response = requests.get(URL)
     print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
@@ -12,16 +12,17 @@ def fetch_and_print_posts():
             print(post["title"])
 
 def fetch_and_save_posts():
-    respons = requests.get(URL)
+    response = requests.get(URL)
     if response.status_code == 200:
         posts = response.json()
-    structured_data = []
-    for post in posts:
-        structured_data.append({
-            "id": post["id"],
-            "title": post["title"],
-            "body": post["body"]
-    with open("posts.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.DictWriter(file. fieldnames = ["id", "title", "body"])
-    writer.writeheader()
-    writer.writerows(structured_data)
+        structured_data = []
+        for post in posts:
+            structured_data.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames = ["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(structured_data)
