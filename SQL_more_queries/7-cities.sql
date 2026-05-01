9239b351a1fc8d4d2bd9e2dfc3501925a4fd4c0e
atakhan11 <atakhanwork@gmail.com>
2026-05-01 12:54:12 +0000

    7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates database hbtn_0d_usa and tables states and cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
