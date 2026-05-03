bf0baa0e3fe316ccf7adf8cd04418b856a518081
atakhan11 <atakhanwork@gmail.com>
2026-05-03 09:26:47 +0000

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for Selecting cities"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT c.name, s.name \
+                    FROM cities c \
+                    INNER JOIN states s ON s.id = c.state_id ")
+    cities = []
+    for city in cur.fetchall():
+        if city[1] == argv[4]:
+            cities.append(city[0])
+    print(", ".join(cities))
+    cur.close()
+    db.close()
